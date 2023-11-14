9590d5977ac4f5774289325502010aef5c3f6634
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-14 03:02:50 -1200

    Update gc-admins.js

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -4,12 +4,12 @@ const handler = async (m, {conn, participants, groupMetadata, args}) => {
   const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
   const pesan = args.join` `;
-  const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  const text = `*━「* 𝐈𝐍𝐕𝐎𝐂𝐀𝐍𝐃𝐎 𝐀𝐃𝐌𝐈𝐍𝐒 *」━*
+  const oi = `*EY ADMIN FIJATE EL GRUPO HIJO DE RE MIL PUTAAA * ${pesan}`;
+  const text = `*━「*𝘽𝙤𝙩𝙨𝙞𝙩𝙤 𝙎𝙖𝙢𝙪𝙧𝙖𝙞 𝙄𝙣𝙫𝙤𝙘𝙖*」━*
 
 ${oi}
 
-*𝙰𝙳𝙼𝙸𝙽𝚂:*
+*ADMINS GUAP@S:*
 ${listAdmin}
 
 *[ ⚠ ️] 𝚄𝚂𝙰𝚁 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚂𝙾𝙻𝙾 𝙲𝚄𝙰𝙽𝙳𝙾 𝚂𝙴 𝚃𝚁𝙰𝚃𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙴𝙼𝙴𝚁𝙶𝙴𝙽𝙲𝙸𝙰!!*`.trim();
