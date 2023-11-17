de516863dceb35adcf97a3c5bc4de4c465c39911
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-17 01:55:16 -0500

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ import moment from 'moment-timezone';
 global.botnumber = ""
 
 global.owner = [
-  ['51906528530', '☃️ Violet - Bot ☃️', true],
+  ['51906528530', '☃️ Samurai - Bot ☃️', true],
   ['50499698072', '💫 Samurai - Collaborator 1 💫', true],
   ['51940617554', '💫 Samurai - Collaborator 2 💫', true],
   ['51996089079', '💫 Samurai - Collaborator 3 💫', true],
@@ -37,11 +37,11 @@ global.suittag = ['51906529530'];
 global.prems = ['51906528530'];
 
 global.packname = 'Sticker';
-global.author = '‧˚꒰🩵 𝗩𝗶𝗼𝗹𝗲𝘁 𝗘𝘃𝗲𝗿𝗴𝗮𝗿𝗱𝗲𝗻 🩵꒱°‧';
-global.wm = '‧˚꒰🩵 𝗩𝗶𝗼𝗹𝗲𝘁 𝗘𝘃𝗲𝗿𝗴𝗮𝗿𝗱𝗲𝗻 🩵꒱°‧';
-global.titulowm = '‧˚꒰🩵 𝗩𝗶𝗼𝗹𝗲𝘁 𝗘𝘃𝗲𝗿𝗴𝗮𝗿𝗱𝗲𝗻 🩵꒱°‧';
-global.titulowm2 = `‧˚꒰🩵 𝗩𝗶𝗼𝗹𝗲𝘁 𝗘𝘃𝗲𝗿𝗴𝗮𝗿𝗱𝗲𝗻 🩵꒱°‧`
-global.igfg = '‧˚꒰🩵 𝗩𝗶𝗼𝗹𝗲𝘁 𝗘𝘃𝗲𝗿𝗴𝗮𝗿𝗱𝗲𝗻 🩵꒱°‧';
+global.author = '🦆 𝙋𝘼𝙏𝙊 𝘽𝙊𝙏 🦆';
+global.wm = '🦆 𝙋𝘼𝙏𝙊 𝘽𝙊𝙏 🦆';
+global.titulowm = '🦆 𝙋𝘼𝙏𝙊 𝘽𝙊𝙏 🦆';
+global.titulowm2 = `🦆 𝙋𝘼𝙏𝙊 𝘽𝙊𝙏 🦆`
+global.igfg = '🦆 𝙋𝘼𝙏𝙊 𝘽𝙊𝙏 🦆';
 global.wait = '*[ 🎄 ] Cargando...*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
@@ -62,9 +62,9 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `▸ ${dia} ${fecha}\n▸ ‧˚꒰🩵 𝗩𝗶𝗼𝗹𝗲𝘁 𝗘𝘃𝗲𝗿𝗴𝗮𝗿𝗱𝗲𝗻 🩵꒱°‧`;
-global.gt = '‧˚꒰🩵 𝗩𝗶𝗼𝗹𝗲𝘁 𝗘𝘃𝗲𝗿𝗴𝗮𝗿𝗱𝗲𝗻 🩵꒱°‧';
-global.mysticbot = '‧˚꒰🩵 𝗩𝗶𝗼𝗹𝗲𝘁 𝗘𝘃𝗲𝗿𝗴𝗮𝗿𝗱𝗲𝗻 🩵꒱°‧';
+global.wm2 = `▸ ${dia} ${fecha}\n▸ 🦆 𝙋𝘼𝙏𝙊 𝘽𝙊𝙏 🦆`;
+global.gt = '🦆 𝙋𝘼𝙏𝙊 𝘽𝙊𝙏 🦆';
+global.mysticbot = '🦆 𝙋𝘼𝙏𝙊 𝘽𝙊𝙏 🦆';
 global.md = 'https://github.com/Bots/RobarNo-MD';
 global.mysticbot = 'https://github.com/Bots/RobarNo-MD';
 global.waitt = '*[ 🎄 ] Cargando...*';
