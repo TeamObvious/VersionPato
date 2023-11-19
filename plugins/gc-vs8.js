545de19dc3baf082297ef3ece10235f4bd24433d
team obvious <149536321+TeamObvious@users.noreply.github.com>
2023-11-19 08:29:06 -1200

    Add files via upload

---
+++ plugins/gc-vs8.js
@@ -0,0 +1,34 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.vs8);
+};
+handler.command = /^(Vs8|vs8)$/i;
+export default handler;
+
+global.vs8 = `
+*⚜️ VS OFICIAL - 8 VS 8⚜️*
+
+_HORA :_
+•10 Pm 🇵🇪
+•12 Am 🇦🇷🇨🇱
+
+*👑 ESCUADRA 1 👑*
+
+📱 
+📱 
+📱 
+📱 
+
+*👑 ESCUADRA 2 👑*
+
+📱
+📱
+📱
+📱
+
+*⚠️SUPLENTES⚠️*
+
+📱 
+📱
+📱
+📱
+`;
---
+++ plugins/gc-vv2.js
@@ -0,0 +1,41 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.Vivido);
+};
+handler.command = /^(Vivido|vivido|Vv2|vv2)$/i;
+export default handler;
+
+global.Vivido = `
+*Reglas Vv2 By: Samurai Bot*
+
+
+*CONFIG SALA*
+
+*TIENDA :* _Avanzada_
+*MONEDAS :* _9999_
+*HP :* _200_
+*MUNICIONES :* _SI_
+*AIDROP Y AIDROP CIBERNETICO :*  _NO_
+*EMULADOR :* _A COORDINAR_
+
+*PERMITIDO*
+
+_NOTA : SI DESEAS AGG ALGO LO AGGS XD_
+
+• *ALTURAS :* _CONTENEDORES , CAJAS , AUTOS , 2DO PISOS_
+
+*ACLARACIONES*
+
+• *HABILIDADES A :* _ALOK , K_
+• *HABILIDADES P :* _MAXIN , MOCO , KELLY , HAYATO_
+• _1RA RONDA DESERT , MINI UZI O M500_
+• *ALTURA PROHIBIDA :* _2do Piso , Montañas ( MILL CLOCK Y OBSERVATORIO No Cuentan Como Altura)_
+• _1 M10 X EQUIPO_
+• _PC A COORDINAR_
+• _2 CAMBIOS POR EQUIPO_
+• _1 ESPEC X EQUIPO_
+• _10 DE TOLERANCIA DESPUES DE MANDAR ID_
+• *ARMAS PERMITIDAS :* _ACA PONES LAS ARMAS PERMITIDAS POR TI_
+
+
+*BY: SAMURAI BOT*
+`;
